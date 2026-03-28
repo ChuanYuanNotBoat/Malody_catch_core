2514bc3ac315ec0671c649c350c0886743e64e99
ミスティア・ローレライ <3178922293@qq.com>
2026-03-28 16:05:32 +0800

    Alpha v0.0.11 --fixed skin load &fixed time div

--- src/model/Skin.cpp
+++ src/model/Skin.cpp
@@ -2,77 +2,99 @@
 #include <QDir>
 #include <QFileInfo>
 #include <QImageReader>
+#include <QDebug>
+#include "utils/Logger.h"
 
 Skin::Skin() : m_valid(false) {}
 
-bool Skin::loadFromDir(const QString& skinPath) {
+bool Skin::loadFromDir(const QString& skinPath)
+{
     clear();
     QDir dir(skinPath);
-    if (!dir.exists()) return false;
-
-    // 简单加载：尝试加载 catch-note-*.png
-    // 这里仅作示例，实际需要根据规则映射
-    QImageReader reader;
-    // 加载 note 类型映射
-    // 0: catch-note-0.png
-    QFileInfo note0(dir.filePath("catch-note-0.png"));
-    if (note0.exists()) {
-        m_notePixmaps[0] = QPixmap(note0.filePath());
+    if (!dir.exists()) {
+        Logger::warn(QString("Skin path does not exist: %1").arg(skinPath));
+        return false;
     }
-    // 1: catch-note-1.png
-    QFileInfo note1(dir.filePath("catch-note-1.png"));
-    if (note1.exists()) m_notePixmaps[1] = QPixmap(note1.filePath());
-    // 2: catch-note-2.png
-    QFileInfo note2(dir.filePath("catch-note-2.png"));
-    if (note2.exists()) m_notePixmaps[2] = QPixmap(note2.filePath());
-    // 3: catch-note-3.png (用于 1/8,1/16,1/32)
-    QFileInfo note3(dir.filePath("catch-note-3.png"));
-    if (note3.exists()) m_notePixmaps[3] = QPixmap(note3.filePath());
-    // 4: catch-note-4.png (用于 1/3,1/6,1/12,1/24)
-    QFileInfo note4(dir.filePath("catch-note-4.png"));
-    if (note4.exists()) m_notePixmaps[4] = QPixmap(note4.filePath());
-    // 5: catch-note-5.png (rain)
-    QFileInfo note5(dir.filePath("catch-note-5.png"));
-    if (note5.exists()) m_notePixmaps[5] = QPixmap(note5.filePath());
 
-    // 加载 bar
-    QFileInfo bar(dir.filePath("catch-bar.png"));
-    if (bar.exists()) m_barPixmap = QPixmap(bar.filePath());
+    // 加载 note 图片映射
+    // 注意文件名约定：catch-note-0.png 对应 1/1 等
+    auto loadNotePixmap = [&](int type, const QString& filename) {
+        QFileInfo info(dir.filePath(filename));
+        if (info.exists()) {
+            m_notePixmaps[type] = QPixmap(info.filePath());
+            if (m_notePixmaps[type].isNull())
+                Logger::warn(QString("Failed to load %1").arg(info.filePath()));
+            else
+                Logger::debug(QString("Loaded note type %1: %2").arg(type).arg(filename));
+        } else {
+            Logger::debug(QString("Note skin %1 not found, will use fallback").arg(filename));
+        }
+    };
 
-    // 加载 light 图片
+    loadNotePixmap(0, "catch-note-0.png");
+    loadNotePixmap(1, "catch-note-1.png");
+    loadNotePixmap(2, "catch-note-2.png");
+    loadNotePixmap(3, "catch-note-3.png");
+    loadNotePixmap(4, "catch-note-4.png");
+    loadNotePixmap(5, "catch-note-5.png");
+
+    // 加载 bar 图片
+    QFileInfo barInfo(dir.filePath("catch-bar.png"));
+    if (barInfo.exists()) {
+        m_barPixmap = QPixmap(barInfo.filePath());
+        Logger::debug("Loaded catch-bar.png");
+    }
+
+    // 加载 light 图片（0-16 及 arc/bar）
     for (int i = 0; i <= 16; ++i) {
-        QFileInfo light(dir.filePath(QString("catch-light-%1.png").arg(i)));
-        if (light.exists()) m_lightPixmaps[i] = QPixmap(light.filePath());
+        QFileInfo lightInfo(dir.filePath(QString("catch-light-%1.png").arg(i)));
+        if (lightInfo.exists()) {
+            m_lightPixmaps[i] = QPixmap(lightInfo.filePath());
+        }
     }
-    // 其他 light 变体
     QFileInfo lightArc(dir.filePath("catch-light-arc.png"));
-    if (lightArc.exists()) m_lightPixmaps[17] = QPixmap(lightArc.filePath());
+    if (lightArc.exists()) {
+        m_lightPixmaps[17] = QPixmap(lightArc.filePath());
+        Logger::debug("Loaded catch-light-arc.png");
+    }
     QFileInfo lightBar(dir.filePath("catch-light-bar.png"));
-    if (lightBar.exists()) m_lightPixmaps[18] = QPixmap(lightBar.filePath());
+    if (lightBar.exists()) {
+        m_lightPixmaps[18] = QPixmap(lightBar.filePath());
+        Logger::debug("Loaded catch-light-bar.png");
+    }
 
     m_valid = true;
+    Logger::info(QString("Skin loaded from %1, note pixmaps count: %2, bar: %3, light: %4")
+                 .arg(skinPath)
+                 .arg(m_notePixmaps.size())
+                 .arg(m_barPixmap.isNull() ? 0 : 1)
+                 .arg(m_lightPixmaps.size()));
     return true;
 }
 
-void Skin::clear() {
+void Skin::clear()
+{
     m_notePixmaps.clear();
     m_barPixmap = QPixmap();
     m_lightPixmaps.clear();
     m_valid = false;
 }
 
-const QPixmap* Skin::getNotePixmap(int noteType) const {
+const QPixmap* Skin::getNotePixmap(int noteType) const
+{
     auto it = m_notePixmaps.constFind(noteType);
     if (it != m_notePixmaps.constEnd())
         return &it.value();
     return nullptr;
 }
 
-const QPixmap* Skin::getBarPixmap() const {
+const QPixmap* Skin::getBarPixmap() const
+{
     return m_barPixmap.isNull() ? nullptr : &m_barPixmap;
 }
 
-const QPixmap* Skin::getLightPixmap(int lightIndex) const {
+const QPixmap* Skin::getLightPixmap(int lightIndex) const
+{
     auto it = m_lightPixmaps.constFind(lightIndex);
     if (it != m_lightPixmaps.constEnd())
         return &it.value();
